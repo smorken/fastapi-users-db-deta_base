a49f17aeb8d6c6ffd8e365b082a3edff53f28046
Scott Morken <scottm361@gmail.com>
2021-09-27 22:16:19 -0700

    black formatting

--- fastapi_users_db_deta_base/__init__.py
+++ fastapi_users_db_deta_base/__init__.py
@@ -7,6 +7,7 @@ from pydantic import UUID4
 from fastapi_users.db.base import BaseUserDatabase
 from fastapi_users.models import UD
 from fastapi_users_db_deta_base import deta_base_async
+
 __version__ = "0.0.0"
 
 
@@ -15,14 +16,10 @@ class DetaBaseUserDatabase(BaseUserDatabase[UD]):
     Database adapter for Deta Base.
 
     :param user_db_model: Pydantic model of a DB representation of a user.
-    :param async_deta_base: deta base object with async method wrappers
+    :param async_deta_base: async deta base
     """
 
-    def __init__(
-        self,
-        user_db_model: Type[UD],
-        async_deta_base
-    ):
+    def __init__(self, user_db_model: Type[UD], async_deta_base):
         super().__init__(user_db_model)
         self.async_deta_base = async_deta_base
 
@@ -34,9 +31,8 @@ class DetaBaseUserDatabase(BaseUserDatabase[UD]):
     async def get_by_email(self, email: str) -> Optional[UD]:
         """Get a single user by email."""
         user = await deta_base_async.looped_fetch(
-            self.async_deta_base.fetch,
-            return_first_match=True,
-            query={"email": email.lower()})
+            self.async_deta_base.fetch, query={"email": email.lower()}
+        )
 
         return self.user_db_model(**user) if user else None
 
@@ -44,11 +40,11 @@ class DetaBaseUserDatabase(BaseUserDatabase[UD]):
         """Get a single user by OAuth account id."""
         user = await deta_base_async.looped_fetch(
             self.async_deta_base.fetch,
-            return_first_match=True,
             query={
                 "oauth_accounts.oauth_name": oauth,
                 "oauth_accounts.account_id": account_id,
-            })
+            },
+        )
 
         return self.user_db_model(**user) if user else None
 
@@ -58,19 +54,16 @@ class DetaBaseUserDatabase(BaseUserDatabase[UD]):
         data["email"] = data["email"].lower()
         if await self.get_by_email(data["email"]):
             raise ValueError()
-        await self.async_deta_base.insert(
-            data=user.dict(), key=user.id)
+        await self.async_deta_base.insert(data=user.dict(), key=user.id)
         return user
 
     async def update(self, user: UD) -> UD:
         """Update a user."""
         data = user.dict()
         data["email"] = data["email"].lower()
-        await self.async_deta_base.put(
-            data=user.dict(), key=user.id)
+        await self.async_deta_base.put(data=user.dict(), key=user.id)
         return user
 
     async def delete(self, user: UD) -> None:
         """Delete a user."""
-        await self.async_deta_base.delete(
-            key=user.id)
+        await self.async_deta_base.delete(key=user.id)
